07d4850b5507f409396e3f6a14124dfd930c4a57
Arun Persaud <arun@nubati.net>
2015-09-26 18:56:45 -0700

    need_update shouldn't be changed during cleanup

--- dlp3.py
+++ dlp3.py
@@ -204,7 +204,7 @@ class myCMD(cmd.Cmd):
         self.packages = []
         self.good_packages = []
         self.bad_packages = []
-        self.need_update = []
+        self.need_update = {}
         self.good = 0
         self.bad = 0
         self.building = 0
@@ -388,7 +388,6 @@ class myCMD(cmd.Cmd):
             if p in self.packages: packages.remove(p)
             if p in self.good_packages: self.good_packages.remove(p)
             if p in self.bad_packages: self.bad_packages.remove(p)
-            if p in self.need_update: self.need_update.remove(p)
 
         if len(packages) == 0:
             print("Nothing to clean up")
