6d2b51e1b6fced1a590c9fdee5d2de75274b0dbc
Arun Persaud <arun@nubati.net>
2016-01-25 21:56:07 -0800

    don't print out information before packages have returned their status

--- dlp3.py
+++ dlp3.py
@@ -484,11 +484,11 @@ class myCMD(cmd.Cmd):
         self.longestname = max([len(p) for p in tocheck]) if len(tocheck) > 0 else 0
 
         # parallel check
+        fut = [pool.submit(self.check_package, p) for p in tocheck]
+        concurrent.futures.wait(fut)
         if self.longestname > 0:
             print("{:^{length}} good bad building".
                   format("name", length=self.longestname+2))
-        fut = [pool.submit(self.check_package, p) for p in tocheck]
-        concurrent.futures.wait(fut)
 
         result = []
         for f in fut:
