34baf3792ee898542ec70e8ebb0b371d0ee08ad3
Arun Persaud <arun@nubati.net>
2016-07-09 09:13:01 -0700

    add auto delete of sha1 from git log

--- OBSchanges-mode.el
+++ OBSchanges-mode.el
@@ -1,5 +1,5 @@
-(defun changelog-format-entry ()
-  "Format a changelog entry as a level 1 item starting with a '  *'.
+(defun changelog-format-entry (itemstring)
+  "Format a changelog entry as a level 1 item starting with itemstring.
   Also: replace `` quotes with a normal quote, delete blank lines after entry,
         add single blank line at end of section"
   (interactive)
@@ -7,9 +7,11 @@
   (delete-whitespace-rectangle (point) (line-end-position) nil)
   (if (looking-at "[-+*] ")
       (delete-char 2))
+  (if (looking-at "[a-z0-9]\{40\} ")
+      (delete-char 41))
   (replace-string "``" "\"" nil (point) (line-end-position))
   (beginning-of-line)
-  (insert "  * ")
+  (insert itemstring)
   (move-end-of-line nil)
   (fill-region (line-beginning-position) (line-end-position))
   (forward-line 1)
@@ -21,28 +23,13 @@
       (insert "\n"))
   )
 
-(defun changelog-format-entry2 ()
-  "Format a changelog entry as a level 2 item starting with a '    +'.
-  Also: replace `` quotes with a normal quote, delete blank lines after entry,
-        add single blank line at end of section"
+(defun changelog-format-level1 ()
   (interactive)
-  (beginning-of-line)
-  (delete-whitespace-rectangle (point) (line-end-position) nil)
-  (if (looking-at "[-+*] ")
-      (delete-char 2))
-  (replace-string "``" "\"" nil (point) (line-end-position))
-  (beginning-of-line)
-  (insert "    + ")
-  (move-end-of-line nil)
-  (fill-region (line-beginning-position) (line-end-position))
-  (forward-line 1)
-    (if (looking-at "[[:blank:]]*\n")
-      (delete-blank-lines))
-  (if (looking-at "- changes from version ")
-      (insert "\n"))
-  (if (looking-at "-------")
-      (insert "\n"))
-  )
+  (changelog-format-entry "  * "))
+
+(defun changelog-format-level2 ()
+  (interactive)
+  (changelog-format-entry "    + "))
 
 (defun changelog-insert-version ()
   (interactive)
@@ -59,8 +46,8 @@
   "Minor mode to make it easier to edit changes files for OBS. Especially to cut&paste from Changelog files and webpages."
   :lighter " OBSchanges"
   :keymap (let ((map (make-sparse-keymap)))
-	    (define-key map (kbd "<f5>") 'changelog-format-entry)
-	    (define-key map (kbd "<f6>") 'changelog-format-entry2)
+	    (define-key map (kbd "<f5>") 'changelog-format-level1)
+	    (define-key map (kbd "<f6>") 'changelog-format-level2)
 	    (define-key map (kbd "<f7>") 'changelog-insert-version)
 	    map)
   (make-variable-buffer-local 'adaptive-fill-regexp)
